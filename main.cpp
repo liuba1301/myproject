5779dceb49514ff98d54eadcdf5a738e53681b12
liuba1301 <lbaranova840@gmail.com>
2024-06-17 00:46:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+
+    std::cout << "Введите ваше имя: ";
+    std::cin >> name;
+
+    std::cout << "Привет, " << name << "!" << std::endl;
+
+    return 0;
+}
+
